490de53a1a4bd057f47fa78fd4c3cbeadfe06183
TorqueReborn <torquereborn@gmail.com>
2025-10-08 22:39:48 +0530

    Add delete icons using ng-icons and stylize it

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { FormsModule } from '@angular/forms'
 import { Component, signal } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { matDeleteForeverSharp } from '@ng-icons/material-icons/sharp';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule],
+  imports: [FormsModule, NgIcon],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  viewProviders: [provideIcons({ matDeleteForeverSharp })]
 })
 export class App {
   protected readonly title = signal('todo');
